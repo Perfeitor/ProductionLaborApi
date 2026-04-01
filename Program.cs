378264fcb880646d423e6f46728b3f2f372dd1a1
Nguyen Quang Huy <quanghuy19062002@gmail.com>
2026-04-01 17:11:12 +0700

    feat: add db connect

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ProductionLaborApi.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ApplicationDbContext>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseNpgsql(configuration.GetConnectionString("DefaultConnection") ?? "")
+        .Options;
+    return new ApplicationDbContext(options);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
