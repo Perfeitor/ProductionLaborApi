f6f747247521b26263d50851cf5274488f93b73c
Nguyen Quang Huy <quanghuy19062002@gmail.com>
2026-04-01 16:06:21 +0700

    feat: init project

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionLaborApi.Controllers;
+
+[ApiController]
+[Route("api/nhanvien")]
+public class EmployeesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok("Hello World");
+    }
+}
